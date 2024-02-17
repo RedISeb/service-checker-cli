880fab4ea723253a123a3c2583105991882a6868
Sebastian Hoffmann <info@rediseb.de>
2024-02-17 17:51:25 +0100

    added check for open tcp ports using netcat on unix systems

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os/exec"
 	"runtime"
 )
+
+var Port string = "80"
 
 func main() {
 	hostPointer := flag.String("host", "localhost", "Hostname or IP address where the service runs")
@@ -13,13 +16,16 @@ func main() {
 	host := *hostPointer
 	fmt.Printf("Check the host: %s\n", host)
 	isAlive := pingHost(host)
+	fmt.Printf("%s is up and running \n", host)
 	if isAlive {
 		var osSystem string = getSystemInfo()
 		switch osSystem {
 		case "darwin":
-			fmt.Println("Mac OS")
+			log.Println("Mac OS")
+			outputDialogTCP(Port, checkTcpPortUnix(host, Port))
+
 		case "linux":
-			fmt.Println("Linux")
+			log.Println("Linux")
 		default:
 			fmt.Printf("%s.\n", osSystem)
 		}
@@ -42,4 +48,24 @@ func pingHost(hostname string) bool {
 	execPing := exec.Command(app, arg1, arg2, arg3)
 	_, err := execPing.Output()
 	return err == nil
+}
+
+func checkTcpPortUnix(hostname string, port string) bool {
+	app := "nc"
+	arg1 := "-vz"
+	arg2 := hostname
+	arg3 := port
+
+	execTest := exec.Command(app, arg1, arg2, string(arg3))
+	_, err := execTest.Output()
+	return err == nil
+}
+
+func outputDialogTCP(port string, isOpenTCP bool) {
+	switch isOpenTCP {
+	case true:
+		fmt.Printf("Service on port %s is running.\n", port)
+	case false:
+		fmt.Printf("Service on port %s is down.\n", port)
+	}
 }
