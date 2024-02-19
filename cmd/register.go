1c478b942d6d1c686110314171a98a921e1d3e20
Sebastian Hoffmann <info@rediseb.de>
2024-02-19 17:44:55 +0100

    added functionality to the register command to add a new service to the default.json file.

--- cmd/register.go
+++ cmd/register.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -21,7 +22,27 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("register called")
+		name, _ := cmd.Flags().GetString("name")
+		host, _ := cmd.Flags().GetString("host")
+		port, _ := cmd.Flags().GetString("port")
+
+		newService := Service{}
+		newService.Name = name
+		newService.Host = host
+		newService.Port = port
+
+		services := ReadDefaultConfig()
+		services.Services = append(services.Services, newService)
+		updatedData, err := json.MarshalIndent(services, "", "    ")
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		err = os.WriteFile("./config/default.json", updatedData, 0644)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
 	},
 }
 
@@ -37,4 +58,9 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	registerCmd.PersistentFlags().StringP("host", "H", "localhost", "Hostname or ip address of the service.")
+	registerCmd.MarkPersistentFlagRequired("host")
+	host, _ := registerCmd.Flags().GetString("host")
+	registerCmd.PersistentFlags().StringP("name", "n", host, "Name for the registered service.")
+	registerCmd.PersistentFlags().StringP("port", "p", "80", "Specify the TCP port of the registered service.")
 }
